978c0b6562683961fc3198bb41d2e8441871d450
LAKRAD Hicham <lakrad.hicham159@gmail.com>
2023-03-17 16:53:45 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Print alphabets lowercase"
+ * Return: 0
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		pitchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
