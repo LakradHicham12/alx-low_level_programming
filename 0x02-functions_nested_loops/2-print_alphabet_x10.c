8c3cc943892d595dc6d53cb0fdcaf3c198192e60
LAKRAD Hicham <lakrad.hicham159@gmail.com>
2023-03-21 17:46:50 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include"main.h"
 /**
- * Description: print_alphapet_x10 - function
- *	that will print the alphabet 10 times
+ * main - check the code.
  *
+ * Return: Always 0.
 */
 
 void print_alphabet_x10(void)
