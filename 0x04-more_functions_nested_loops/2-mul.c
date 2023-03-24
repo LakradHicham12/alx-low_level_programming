57e886c5fdba799eff98c2491d19db8b1b9bb38d
LAKRAD Hicham <lakrad.hicham159@gmail.com>
2023-03-24 06:10:45 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+# inclure  < stdio.h >
+# inclure  " main.h "
+/* *
+* mul - multiplie deux entiers
+* @a : premier entier
+* @b : deuxième entier
+* Retour : addition de a et b
+*/
+int  mul ( int a, int b)
+{
+	retour (a * b);
+}
