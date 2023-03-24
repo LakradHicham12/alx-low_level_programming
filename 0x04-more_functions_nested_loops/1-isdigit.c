db2ee1b6fd0f32055f510d385ec96a819e455859
LAKRAD Hicham <lakrad.hicham159@gmail.com>
2023-03-24 06:09:51 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+# inclure  " main.h "
+
+/* *
+* _isdigit - écrivez une fonction qui vérifie un chiffre (0 à 9).
+* @c : caractère à vérifier
+* Retour : 0 ou 1
+* */
+entier  _isdigit ( entier c)
+{
+	si (c >= ' 0 ' && c <= ' 9 ' )
+		retour ( 1 );
+	autre
+		retour ( 0 );
+}
+
