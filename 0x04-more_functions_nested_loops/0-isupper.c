4bbe0817350a769df4a00b3e4d3d0f168a9a3b5f
LAKRAD Hicham <lakrad.hicham159@gmail.com>
2023-03-24 06:08:47 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+# inclure  " main.h "
+
+/* *
+* _isupper - lettres majuscules
+* @c : caractère à vérifier
+*
+* Retour : 0 ou 1
+ */
+int  _isupper ( int c)
+
+{
+	si (c >= ' A ' && c <= ' Z ' )
+		retour ( 1 );
+	autre
+		retour ( 0 );
+
+}
