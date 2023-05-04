add4e1c1baf9c2f78bff73886794f17c495dbb1c
LAKRAD Hicham <lakrad.hicham159@gmail.com>
2023-05-04 14:48:52 -0700

    task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = sizeof(n) * 8, printed = 0;
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed)
+			_putchar('0');
+	}
+	if (!printed)
+		_putchar('0');
+}
