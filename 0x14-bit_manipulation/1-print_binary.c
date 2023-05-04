73bd88ed921a1158c7ecaf0c83b31e36f45431bf
LAKRAD Hicham <lakrad.hicham159@gmail.com>
2023-05-04 14:56:36 -0700

    task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,7 @@
  *
  * Return: value of the bit.
  */
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int n)
 {
 	int bit = sizeof(n) * 8, printed = 0;
 	while (bit)
